e8cc74c9962edbee81295088440249050ecd54a4
ZQuek <zackquek@hotmail.com>
2025-06-03 19:03:20 -0500

    updated FPS to 60

--- main.py
+++ main.py
@@ -6,10 +6,13 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     pg.init()
+    timer = pg.time.Clock()
+    dt = 0
     screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         screen.fill((0,0,0))
         pg.display.flip()
+        dt = timer.tick(60)/1000
         
         for event in pg.event.get():
             if event.type == pg.QUIT:
